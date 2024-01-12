2f8a747878205f7a05aabe4f6a0e56bedd021156
James Shea <jamesshea@Admins-MacBook-Pro.local>
2024-01-12 14:59:24 +0000

    homework week2

---
+++ hero.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+    for( let a = 0; a < 4; a++){
+        document.getElementById(`hero${a}`).onclick = function () {
+            document.getElementById(`hero${a}`).style.backgroundColor = "yellow";
+            for(let i = 0; i < 4; i++){
+                if(i != a){
+                    document.getElementById(`hero${i}`).style.backgroundColor = "white";
+                }
+            }
+        }
+    }
+};
+
