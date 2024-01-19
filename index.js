cd4ce990d1380f84d8f5aaeb99b71ad7bf70ffea
Mihai Sarbu <mihaisarbu@Mihais-MacBook-Pro.local>
2024-01-19 11:13:53 +0000

    lesson 1 changes

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+window.onload = () => {
+    for (let a = 0; a < 4; a++) {
+        document.getElementById(`class${a}`).onclick = function () {
+            document.getElementById(`class${a}`).style.backgroundColor = "yellow";
+            for (let i = 0; i < 4; i++) {
+                if (i != a) {
+                    document.getElementById(`class${i}`).style.backgroundColor = "white";
+                }
+            }
+        };
+    }
+};
